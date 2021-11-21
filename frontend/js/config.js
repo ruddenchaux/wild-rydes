2912ea15fc53355a745af2da7383e56c329f979a
Federico Dainelli <f.r.dainelli@gmail.com>
2021-11-21 18:46:58 +0100

    add cognito poll

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "eu-west-1_TafT30ndP",
-    userPoolClientId: "506lsjtkmsfubgo67if0g7tjgi",
+    userPoolId: "eu-west-1_35MaX7j73",
+    userPoolClientId: "3tq4io29ltqfmle60uma8icaeb",
     region: "eu-west-1",
   },
   api: {
-    invokeUrl: "https://kl8fhq4h3g.execute-api.eu-west-1.amazonaws.com/prod",
+    invokeUrl: "",
   },
 };
--- lib/wild-rydes-stack.ts
+++ lib/wild-rydes-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from '@aws-cdk/aws-amplify';
 import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as cognito from '@aws-cdk/aws-cognito';
 
 export class WildRydesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // create amplify to host front end
     const amplifyApp = new amplify.App(this, 'WildRydes', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: 'ruddenchaux',
@@ -17,22 +19,40 @@ export class WildRydesStack extends cdk.Stack {
       },
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '1.0',
-        frontend: {
-          appRoot: 'frontend',
-          phases: {
-            build: {
-              commands: []
+        applications: {
+          frontend: {
+            appRoot: 'frontend',
+            phases: {
+              build: {
+                commands: []
+              }
+            },
+            artifacts: {
+              baseDirectory: '/',
+              files:
+              - '**/*'
             }
-          },
-          artifacts: {
-            baseDirectory: '/',
-            files:
-            - '**/*'
           }
         }
       })
     });
 
     amplifyApp.addBranch('master');
+
+    // create cognito to manage the clients auth
+    const pool = new cognito.UserPool(this, 'WildRydesCognito', {
+      userPoolName: 'wildrydes-userpool',
+    });
+
+    // create pool client
+    const poolClient = pool.addClient('WildRydesWebApp', {
+      generateSecret: false
+    });
+
+    // print the cognito pool id
+    new cdk.CfnOutput(this, 'UserPoolId', { value: pool.userPoolId });
+
+    // print the cognito poll client id
+    new cdk.CfnOutput(this, 'UserPoolClientId', { value: poolClient.userPoolClientId });
   }
 }
