6551c5ae6d6b0e80a6d947791d403009ead35d06
Federico Dainelli <f.r.dainelli@gmail.com>
2021-11-21 16:50:59 +0100

    add cdk stack

---
+++ lib/wild-rydes-stack.ts
@@ -0,0 +1,33 @@
+import * as cdk from '@aws-cdk/core';
+import * as amplify from '@aws-cdk/aws-amplify';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+
+export class WildRydesStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const amplifyApp = new amplify.App(this, 'MyApp', {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: 'ruddenchaux',
+        repository: 'wild-rydes',
+        oauthToken: cdk.SecretValue.secretsManager('my-github-token') as any
+      }),
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+        version: '1.0',
+        frontend: {
+          appRoot: "frontend",
+          phases: {
+            build: {
+              commands: []
+            }
+          },
+          artifacts: {
+            baseDirectory: '/',
+            files:
+            - '**/*'
+          }
+        }
+      })
+    });
+  }
+}
---
+++ test/wild-rydes.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as WildRydes from '../lib/wild-rydes-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wild-rydes-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WildRydes.WildRydesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
