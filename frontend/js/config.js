87bd1b37f9dc647923456fad7e8b4308e7fc7f7b
Federico Dainelli <f.r.dainelli@gmail.com>
2021-11-22 19:28:59 +0100

    add api gateway

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "eu-west-1_4x6ZeeOHU",
-    userPoolClientId: "21ckr9lc9u2n589st0pf9o9u5t",
-    region: "eu-west-1",
+    userPoolId: 'eu-west-1_4x6ZeeOHU',
+    userPoolClientId: '21ckr9lc9u2n589st0pf9o9u5t',
+    region: 'eu-west-1'
   },
   api: {
-    invokeUrl: "",
-  },
+    invokeUrl: 'https://17z337l897.execute-api.eu-west-1.amazonaws.com/prod'
+  }
 };
--- lib/wild-rydes-stack.ts
+++ lib/wild-rydes-stack.ts
@@ -6,17 +6,33 @@ import * as cognito from '@aws-cdk/aws-cognito';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as iam from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
-
+import * as apigateway from '@aws-cdk/aws-apigateway';
 export class WildRydesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    this.createAmplifyApp();
+
+    const { userPool, userPoolClient } = this.createCognitoUserPool();
+
+    const table = this.createDynamoDbTable();
+
+    const role = this.createIamRoleForDynamoTableAndLambda(table);
+
+    const lambdaFunction = this.createLambdaFunction(role);
+
+    const api = this.createApiGateway(lambdaFunction, userPool);
+
+    this.printOutput(userPool, userPoolClient, api);
+  }
+
+  private createAmplifyApp(): amplify.App {
     // create amplify to host front end
     const amplifyApp = new amplify.App(this, 'WildRydes', {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: 'ruddenchaux',
         repository: 'wild-rydes',
-        oauthToken: cdk.SecretValue.secretsManager('github-access-token') as any,
+        oauthToken: cdk.SecretValue.secretsManager('github-access-token') as any
       }),
       customRules: [
         {
@@ -35,21 +51,15 @@ export class WildRydesStack extends cdk.Stack {
           {
             frontend: {
               phases: {
-                build: {
-                  commands: []
-                }
+                build: { commands: [] }
               },
               artifacts: {
                 baseDirectory: '/',
-                files: [
-                  '**/*'
-                ]
+                files: ['**/*']
               },
-              cache: {
-                paths: []
-              }
+              cache: { paths: [] }
             },
-            appRoot: 'frontend',
+            appRoot: 'frontend'
           }
         ]
       })
@@ -57,14 +67,16 @@ export class WildRydesStack extends cdk.Stack {
 
     amplifyApp.addBranch('master');
 
+    return amplifyApp;
+  }
+
+  private createCognitoUserPool(): { userPool: cognito.UserPool; userPoolClient: cognito.UserPoolClient } {
     // create cognito to manage the clients auth
-    const pool = new cognito.UserPool(this, 'WildRydesCognito', {
+    const userPool = new cognito.UserPool(this, 'WildRydesCognito', {
       userPoolName: 'wildrydes-userpool',
       signInCaseSensitive: false,
       selfSignUpEnabled: true,
-      autoVerify: {
-        email: true
-      },
+      autoVerify: { email: true },
       email: cognito.UserPoolEmail.withCognito(),
       standardAttributes: {
         email: {
@@ -75,44 +87,79 @@ export class WildRydesStack extends cdk.Stack {
     });
 
     // create pool client
-    const poolClient = pool.addClient('WildRydesWebApp', {
-      generateSecret: false
-    });
+    const userPoolClient = userPool.addClient('WildRydesWebApp', { generateSecret: false });
 
+    return { userPool, userPoolClient };
+  }
+
+  private createDynamoDbTable(): dynamodb.Table {
     // create dynamodb table
-    const table = new dynamodb.Table(this, 'Rides', {
-      partitionKey: { name: 'RideId', type: dynamodb.AttributeType.STRING },
-    });
+    return new dynamodb.Table(this, 'Rides', { partitionKey: { name: 'RideId', type: dynamodb.AttributeType.STRING } });
+  }
 
+  private createIamRoleForDynamoTableAndLambda(table: dynamodb.Table): iam.Role {
     // create role for lambda
-    const role = new iam.Role(this, 'WildRydesLambda', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-    });
+    const role = new iam.Role(this, 'WildRydesLambdaRole', { assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com') });
 
     // create policy to write on dynamodb table
-    role.addToPolicy(new iam.PolicyStatement({
-      resources: [table.tableArn],
-      actions: ['dynamodb:PutItem'],
-    }));
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        resources: [table.tableArn],
+        actions: ['dynamodb:PutItem']
+      })
+    );
 
     // create policy for lambda
-    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'));
 
+    return role;
+  }
+
+  private createLambdaFunction(role: iam.Role): lambda.Function {
     // create lambda
-    const lambdaFn = new lambda.Function(this, 'WildRydesLambda', {
-      runtime: lambda.Runtime.NODEJS_6_10,
+    return new lambda.Function(this, 'WildRydesLambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'requestUnicorn.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../backend')),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       role
     });
+  }
+
+  private createApiGateway(lambdaFunction: lambda.Function, userPool: cognito.UserPool): apigateway.LambdaRestApi {
+    // create the api on api gategay
+    const api = new apigateway.LambdaRestApi(this, 'WildRydesApi', {
+      handler: lambdaFunction,
+      proxy: false,
+      deployOptions: { stageName: 'prod' },
+      defaultCorsPreflightOptions: { allowOrigins: apigateway.Cors.ALL_ORIGINS }
+    });
+
+    // create cognito authorizer for rest api
+    const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'WildRydesApiAuthorizer', {
+      cognitoUserPools: [userPool],
+      identitySource: apigateway.IdentitySource.header('Authorization')
+    });
+
+    // create ride resource
+    const rideResource = api.root.addResource('ride');
+
+    // create method with lambda integration e cognito authorizer
+    rideResource.addMethod('POST', new apigateway.LambdaIntegration(lambdaFunction), {
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+      authorizer
+    });
+
+    return api;
+  }
+
+  private printOutput(userPool: cognito.UserPool, userPoolClient: cognito.UserPoolClient, api: apigateway.LambdaRestApi) {
+    // print the cognito pool id
+    new cdk.CfnOutput(this, 'UserPoolId', { value: userPool.userPoolId });
 
     // print the cognito poll client id
-    new cdk.CfnOutput(this, 'UserPoolClientId', { value: poolClient.userPoolClientId });
+    new cdk.CfnOutput(this, 'UserPoolClientId', { value: userPoolClient.userPoolClientId });
 
-    // print the cognito pool id
-    new cdk.CfnOutput(this, 'UserPoolId', { value: pool.userPoolId });
-
-    // print the cognito pool id
-    // new cdk.CfnOutput(this, 'UserPoolId', { value: lambdaFn. });
+    // print the invoke url of the api
+    new cdk.CfnOutput(this, 'InvokeUrlRestAPI', { value: api.urlForPath('/ride') });
   }
 }
